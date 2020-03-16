941d2c4d9903254ad15a5e52271e9a38314418c1
haram <thdgkfka098@github.com>
2020-03-16 21:25:48 +0900

    JPA update, select 적용 및 테스트코드 작성

---
+++ src/main/java/com/haram/toy/service/posts/PostsService.java
@@ -0,0 +1,35 @@
+package com.haram.toy.service.posts;
+
+import com.haram.toy.domain.posts.Posts;
+import com.haram.toy.domain.posts.PostsRepository;
+import com.haram.toy.web.dto.PostsResponseDto;
+import com.haram.toy.web.dto.PostsSaveRequestDto;
+import com.haram.toy.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto updateRequestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+        posts.update(updateRequestDto.getTitle(), updateRequestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
+    }
+
+}
