e23919e6dd68c38c418925a741deba177b6b43f4
haram <thdgkfka098@github.com>
2020-04-07 22:52:27 +0900

    수정,삭제

--- src/main/java/com/haram/toy/service/posts/PostsService.java
+++ src/main/java/com/haram/toy/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.haram.toy.service.posts;
 
 import com.haram.toy.domain.posts.Posts;
 import com.haram.toy.domain.posts.PostsRepository;
+import com.haram.toy.web.dto.PostsListResponseDto;
 import com.haram.toy.web.dto.PostsResponseDto;
 import com.haram.toy.web.dto.PostsSaveRequestDto;
 import com.haram.toy.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -30,6 +33,18 @@ public class PostsService {
     public PostsResponseDto findById (Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() ->new IllegalArgumentException("해당 사용자가 없습니다. ID = " + id));
+
+        postsRepository.delete(posts);
     }
 
 }
